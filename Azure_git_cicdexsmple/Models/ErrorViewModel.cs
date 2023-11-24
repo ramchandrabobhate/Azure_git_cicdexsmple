75cc4b3c64b35bae3a10514efc73142512083537
ramch <Ramchandrabobhate@gmail.com[200~got config --global user.name Ramchandra>
2023-11-24 20:25:21 +0530

    Add project files.

---
+++ Azure_git_cicdexsmple/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Azure_git_cicdexsmple.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
